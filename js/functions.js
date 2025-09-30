f8cfeafa23808e4cd3d8d4be9d706887c39f63da
rudchenkoai <rudchenkoai@kamgov.ru>
2025-09-30 22:37:35 +1200

    Исправлены ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,28 +1,26 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 function checkLengthString (string, maxLength) {
- return string.length <= maxLength
-};
-console.log (checkLengthString ('Некоторая строка', 20))
-
+  return string.length <= maxLength;
+}
 
 function checkPalindrome (string) {
-normalString = (string.replaceAll(' ','')).toLowerCase();
-reverseString = '';
-for (let i = normalString.length -1; i >=0 ; i--) {
-reverseString += normalString[i]
-};
-return normalString===reverseString;
-};
-
-console.log (checkPalindrome ('дом Мод'));
+  normalString = (string.replaceAll(' ','')).toLowerCase();
+  reverseString = '';
+  for (let i = normalString.length - 1; i >= 0 ; i--) {
+    reverseString += normalString[i];
+  }
+  return normalString === reverseString;
+}
 
 
 function extractNumbers (string) {
-normalString = (string.toString()).replaceAll(' ','');
-stringNumbers = '';
-for (let i = 0; i <= (string.toString()).length ; i++) {
-Number.isNaN(++normalString[i])?  'не число': stringNumbers += normalString[i];
-};
-return parseInt(stringNumbers);
-};
+  normalString = (string.toString()).replaceAll(' ','');
+  stringNumbers = '';
+  for (let i = 0; i <= (string.toString()).length ; i++) {
+    Number.isNaN(++normalString[i]) ? 'не число' : stringNumbers += normalString[i];
+  }
+  return parseInt(stringNumbers, 10);
+}
 
-console.log (extractNumbers ('Жили-были 2 веселых гуся и 11 гусынь'));
